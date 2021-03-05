4a6d88cbe57927f7674747e3e27140b7157696f7
Bekarysalashybayev <bekarysalashybaev@gmail.com>
2021-03-05 22:02:26 +0600

    Added site

--- mainsite/urls.py
+++ mainsite/urls.py
@@ -4,5 +4,9 @@ from mainsite import views
 
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('/insurance', views.insurance, name='insurance'),
+    path('/about', views.about, name='about'),
+    path('/resources', views.resources, name='resources'),
+    path('/contact', views.contact, name='contact'),
 ]
--- mainsite/views.py
+++ mainsite/views.py
@@ -11,3 +11,35 @@ def index(request):
 
     html_template = loader.get_template('main-site/index.html')
     return HttpResponse(html_template.render(context, request))
+
+
+def insurance(request):
+    context = {}
+    context['segment'] = 'insurance'
+
+    html_template = loader.get_template('main-site/insurance.html')
+    return HttpResponse(html_template.render(context, request))
+
+
+def about(request):
+    context = {}
+    context['segment'] = 'about'
+
+    html_template = loader.get_template('main-site/about.html')
+    return HttpResponse(html_template.render(context, request))
+
+
+def resources(request):
+    context = {}
+    context['segment'] = 'resources'
+
+    html_template = loader.get_template('main-site/resource.html')
+    return HttpResponse(html_template.render(context, request))
+
+
+def contact(request):
+    context = {}
+    context['segment'] = 'contact'
+
+    html_template = loader.get_template('main-site/contact.html')
+    return HttpResponse(html_template.render(context, request))
